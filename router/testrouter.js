5c1cfcc9b8d11b8bccd63408d36fba433375bfd7
mister-ard97 <rezardiansyah1997@gmail.com>
2019-10-31 15:53:12 +0700

    'test'

--- router/testrouter.js
+++
@@ -1,8 +0,0 @@
-var express = require('express')
-var router = express.Router();
-
-const { testcontroller} = require('../controller/index')
-
-router.get('/test',testcontroller.getemail)
-
-module.exports = router
